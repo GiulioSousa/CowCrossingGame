7f792a2621f6a144993e0fe280616f7314b11672
GiulioSousa <80715172+GiulioSousa@users.noreply.github.com>
2021-10-03 11:07:17 -0300

    Update sketch.js
    Cleaning code

--- sketch.js
+++ sketch.js
@@ -1,19 +1,3 @@
-let roadImage;
-let playerImage;
-let carImage;
-//carro
-let xCar = 420;
-
-//Player
-let yPlayer = 5;
-let xPlayer = 100;
-
-function preload(){
-  roadImage = loadImage("Imagens/estrada.png");
-  playerImage = loadImage("Imagens/ator-1.png")
-  carImage = loadImage("Imagens/carro-1.png")
-}
-
 function setup() {
   createCanvas(500, 400);
 }
@@ -24,32 +8,4 @@ function draw() {
   showCar();
   moveCar();
   movePlayer();
-}
-
-function showPlayer(){
-  image(playerImage, xPlayer, yPlayer, 25, 25);
-}
-
-function showCar(){
-  image(carImage, xCar, 96, 50, 40);
-}
-
-function moveCar(){
-  xCar -= 3;
-}
-
-function movePlayer(){
-  if (keyIsDown (DOWN_ARROW)) {
-    yPlayer += 3;
-  }
-  if (keyIsDown (UP_ARROW)) {
-    yPlayer -=3;
-  }
-  if (keyIsDown (LEFT_ARROW)) {
-    xPlayer -= 3;
-  }
-  if (keyIsDown (RIGHT_ARROW)) {
-    xPlayer +=3;
-  }
-  
 }
