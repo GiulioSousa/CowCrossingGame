c17bb4c6745a2ffc2ec65098f49a3f2d3df088b3
GiulioSousa <80715172+GiulioSousa@users.noreply.github.com>
2021-10-03 11:02:54 -0300

    Create imagens.js
    Code division by objects - Images

---
+++ imagens.js
@@ -0,0 +1,13 @@
+let roadImage;
+let playerImage;
+let carImage;
+let carImage2;
+let carImage3;
+
+function preload(){
+  roadImage = loadImage("Imagens/estrada.png");
+  playerImage = loadImage("Imagens/ator-1.png");
+  carImage = loadImage("Imagens/carro-1.png");
+  carImage2 = loadImage("Imagens/carro-2.png");
+  carImage3 = loadImage("Imagens/carro-3.png");
+}
