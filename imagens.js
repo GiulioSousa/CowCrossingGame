7b7667f007d0ba666055a677dcb366744e28ffad
GiulioSousa <80715172+GiulioSousa@users.noreply.github.com>
2021-10-03 17:36:19 -0300

    Update imagens.js
    Optimizing with arrays and cleaning code

--- imagens.js
+++ imagens.js
@@ -1,13 +1,17 @@
 let roadImage;
 let playerImage;
 let carImage;
-let carImage2;
-let carImage3;
+
+let soundTrack;
+let soundCrash;
+let soundPoint;
 
 function preload(){
   roadImage = loadImage("Imagens/estrada.png");
   playerImage = loadImage("Imagens/ator-1.png");
-  carImage = loadImage("Imagens/carro-1.png");
-  carImage2 = loadImage("Imagens/carro-2.png");
-  carImage3 = loadImage("Imagens/carro-3.png");
+  carImage = [loadImage("Imagens/carro-4.png"), loadImage("Imagens/carro-5.png"), loadImage("Imagens/carro-6.png"), loadImage("Imagens/carro-7.png"), loadImage("Imagens/carro-8.png"), loadImage("Imagens/carro-9.png")];
+  
+  soundTrack = loadSound("sons/trilha.mp3");
+  soundCrash = loadSound("sons/colidiu.mp3");
+  soundPoint = loadSound("sons/pontos.wav");
 }
